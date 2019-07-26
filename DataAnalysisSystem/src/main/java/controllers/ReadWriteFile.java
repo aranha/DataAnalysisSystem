83e6d38f6184b52e8f42fc12f26d5cca36d67551
aranha <pauloaranhadev@gmail.com>
2019-07-26 15:45:49 -0300

    criada a funcao de leitura de arquivo no diretorio passado lendo arquivos .dat, exceto o do relatorio

--- DataAnalysisSystem/src/main/java/controllers/ReadWriteFile.java
+++ DataAnalysisSystem/src/main/java/controllers/ReadWriteFile.java
@@ -1,16 +1,25 @@
 package controllers;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class ReadWriteFile {
+    private static String OUTPUT_FILE_NAME = "report.dat";
 
     private ReadWriteFile(){}
 
     public static void readFile(){
         String ARCHIVE_NAME = "file.txt";
         String SEPARATOR = "ç";
+        System.out.println(getFileDat());
         try (FileReader reader = new FileReader(ARCHIVE_NAME)){
             BufferedReader buffer = new BufferedReader(reader);
             TypeData typeData = TypeData.getInstance();
@@ -28,12 +37,25 @@ public final class ReadWriteFile {
     }
 
     private static void writeFileReport(){
-        String OUTPUT_FILE_NAME = "report.dat";
         ReportOutput reportOutput = new ReportOutput();
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE_NAME, true))){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE_NAME, false))){
             writer.write(reportOutput.getReport());
         } catch (IOException e) {
             System.err.println("Cannot write a file, cause happened an error + " + e.getMessage());
+        }
+    }
+
+    private static List<File> getFileDat(){
+        String EXTENSION = ".dat";
+        try {
+            return Files.walk(Paths.get("/home/ilegra/Documentos/dataAnalysisSystem/")).
+                    filter(Files::isRegularFile).map(Path::toFile).
+                    filter(file -> file.getName().endsWith(EXTENSION)).
+                    filter(file -> !file.getName().equals(OUTPUT_FILE_NAME)).
+                    collect(Collectors.toList());
+        } catch (IOException e) {
+            System.err.println("directory not found " + e.getMessage());
+            return null;
         }
     }
 }
